a37891a398492740be1cd8e67c09bb3e0d6fcd6e
adis-tek <67308354+adis-tek@users.noreply.github.com>
2021-03-26 18:09:12 -0500

    foundation of api is set and built

--- src/api.js
+++ src/api.js
@@ -1,4 +1,11 @@
 //API key = a9eb28cd3ef74d2baf520bb9c6607ceb
-
+const auth = "a9eb28cd3ef74d2baf520bb9c6607ceb";
+const state_code = "CA";
 // Base URL
-const base_url = `https://api.covidactnow.org/v2/state/${state}.json?apiKey=a9eb28cd3ef74d2baf520bb9c6607ceb`;
+const base_url = `https://api.covidactnow.org/v2`
+// State data
+const state = `/state/${state_code}.timeseries.json?apiKey=${auth}`;
+
+const state_URL = () => `${base_url}${state}`;
+
+console.log(state_URL());
