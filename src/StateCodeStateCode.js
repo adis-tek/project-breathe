7b6284a723883a3009ce6774e2817880964a1828
adis-tek <67308354+adis-tek@users.noreply.github.com>
2021-04-14 01:33:32 -0500

    updated state code to be shown to user

--- src/StateCodeStateCode.js
+++ src/StateCodeStateCode.js
@@ -1,16 +1,23 @@
 import React, { useState, useEffect } from 'react'
 import useLocalStorage from './useLocalStorage';
 import { Grid, Container, Paper, Typography, Card, CardContent, MenuItem, Menu, Select } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import {   Formik, Field, Form, FieldAttributes, FieldArray } from 'formik';
 import { useDispatch, useSelector} from 'react-redux';
 import { loadCodeInitials } from './actions/codeInitialAction';
 import { loadStateData } from './actions/stateAction';
 import { loadStateDataHistory } from './actions/stateDataHistoryAction';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
+
+const useStyles = makeStyles((theme) => ({
+
+}));
 
 function StateCodeStateCode() {
     const dispatch = useDispatch();
-    const [state, setState] = useLocalStorage('name', '')
+    const [state, setState] = useLocalStorage('name', '');
+    const aaa = 12;
 
     useEffect(() => {
         dispatch(loadCodeInitials());
@@ -44,13 +51,14 @@ console.log(codeInitialsTest);
     return (
         <>
         <Formik>
-            <div>
-        <Select variant="outlined" onChange={onStateChange}>
-            <MenuItem value="AL">AL</MenuItem>
-            <MenuItem value="AK" >AK</MenuItem>
-            <MenuItem value="AZ" >AZ</MenuItem>
+            <StateSelector>
+            <h1>State: {state}</h1>
+        <Select style={{minWidth: 200}} variant="outlined" onChange={onStateChange}>
+            <MenuItem value="AL">Alabama</MenuItem>
+            <MenuItem value="AK" >Alaska</MenuItem>
+            <MenuItem value="AZ" >Arizona</MenuItem>
         </Select>
-            </div>
+            </StateSelector>
         </Formik>
     </>
     );
@@ -61,5 +69,27 @@ const mapStateToProps = state => {
         codeInitialsTest: state.codeInitialsTest
     }
 }
+
+const StateSelector = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 25px;
+    border: 1px solid red;
+    width: 100%;
+    height: 20%;
+`;
+
+const State = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 25px;
+    border: 1px solid red;
+    width: 100%;
+    height: 20%;
+`;
 
 export default connect (mapStateToProps) (StateCodeStateCode);
